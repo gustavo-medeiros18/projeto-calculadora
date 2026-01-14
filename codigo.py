a276ec48fece33cb80784bb1d5ffd4746729bf10
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2026-01-13 22:22:45 -0300

    Implementando as funções das operações básicas

---
+++ codigo.py
@@ -0,0 +1,19 @@
+def soma(a, b):
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
+def multiplicacao(a, b):
+    return a * b
+
+def divisao(a, b):
+    return a / b
+
+valor_1 = 10
+valor_2 = 20
+
+print("valor 1 somado com valor 2:", soma(valor_1, valor_2))
+print("valor 1 menos o valor 2:", subtracao(valor_1, valor_2))
+print("valor 1 multiplicado pelo valor 2:", multiplicacao(valor_1, valor_2))
+print("valor 1 dividido pelo valor 2:", divisao(valor_1, valor_2))
